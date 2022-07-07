def706e955a487cb039549de9de4860e79d65ceb
Dima Khludkov <57712402+dmytrokhl@users.noreply.github.com>
2022-07-07 15:16:36 +0300

    Update RN demo. (#4)

---
+++ NativeWrappers/ShowViews/ExpDateLabel.js
@@ -0,0 +1,3 @@
+import {requireNativeComponent} from 'react-native';
+const ExpDateLabel = requireNativeComponent('VGSExpDateLabel');
+export default ExpDateLabel;
---
+++ ios/AwesomeProject/CollectWrappers/CollectViews/VGSTextFieldManager.m
@@ -0,0 +1,13 @@
+//
+//  VGSTextFieldManager.m
+//  AwesomeProject
+//
+
+#import <Foundation/Foundation.h>
+#import "React/RCTViewManager.h"
+
+@interface RCT_EXTERN_MODULE(VGSCardTextFieldManager, RCTViewManager)
+@end
+
+@interface RCT_EXTERN_MODULE(VGSExpDateTextFieldManager, RCTViewManager)
+@end
