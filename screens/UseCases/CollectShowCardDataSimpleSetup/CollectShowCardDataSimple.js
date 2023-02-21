f4e4381ce19e611c5e99177821c4983955899870
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2023-02-21 15:08:25 +0200

    Remove old android view

--- screens/UseCases/CollectShowCardDataSimpleSetup/CollectShowCardDataSimple.js
+++ screens/UseCases/CollectShowCardDataSimpleSetup/CollectShowCardDataSimple.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SafeAreaView, View, Text, ScrollView, StyleSheet} from 'react-native';
 
-import VGSFormView from '../../../NativeWrappers/VGSFormView.ios';
+import VGSFormView from '../../../NativeWrappers/VGSFormView';
 
 function CollectCustomCardDataScreen() {
   return (
