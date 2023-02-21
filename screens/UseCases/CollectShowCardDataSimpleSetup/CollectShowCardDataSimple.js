719220e8680a76d6a651312e4adc5201e042c775
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2023-02-21 15:39:16 +0200

    Remove not used styles

--- screens/UseCases/CollectShowCardDataSimpleSetup/CollectShowCardDataSimple.js
+++ screens/UseCases/CollectShowCardDataSimpleSetup/CollectShowCardDataSimple.js
@@ -8,9 +8,6 @@ function CollectCustomCardDataScreen() {
     <SafeAreaView>
       <ScrollView style={styles.scrollView}>
         <VGSFormView />
-        <Text numberOfLines={0} style={styles.consoleText}>
-          Start with collecting Card Data...
-        </Text>
       </ScrollView>
     </SafeAreaView>
   );
@@ -21,28 +18,5 @@ export default CollectCustomCardDataScreen;
 const styles = StyleSheet.create({
   scrollView: {
     height: '100%',
-  },
-  item: {
-    height: 80,
-    backgroundColor: 'orange',
-    marginHorizontal: 24,
-    marginVertical: 8,
-  },
-  buttons: {
-    marginHorizontal: 24,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  spacerView: {
-    width: 16,
-  },
-  button: {
-    flex: 1,
-  },
-  consoleText: {
-    marginHorizontal: 24,
-    marginTop: 16,
-    marginBottom: 24,
-    flex: 1,
   },
 });
