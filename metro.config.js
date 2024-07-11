56295c8247d8cddfaaf65f5fb134b3a93b4140fe
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2024-07-11 14:54:27 +0300

    Update RN version to 0.72.0

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,10 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 /**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
  *
- * @format
+ * @type {import('metro-config').MetroConfig}
  */
+const config = {};
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
