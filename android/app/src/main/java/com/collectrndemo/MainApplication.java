8ad92ed900bcf8b2e0de16ca1f964c3a0b8086a6
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2024-07-12 11:06:14 +0300

    Update RN version to 0.74.3

--- android/app/src/main/java/com/collectrndemo/MainApplication.java
+++ android/app/src/main/java/com/collectrndemo/MainApplication.java
@@ -75,7 +75,6 @@ public class MainApplication extends Application implements ReactApplication {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
             DefaultNewArchitectureEntryPoint.load();
         }
-        ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
 
     private void getShowPackages(List<ReactPackage> packages) {
